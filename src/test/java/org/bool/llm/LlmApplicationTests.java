4ff4212ee487d3d796172efa82aecd7aea9204fa
maosen <dongmaosen@yeah.net>
2026-08-29 12:41:13 +0800

    初始化

---
+++ src/test/java/org/bool/llm/LlmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bool.llm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
